e20a8f8c3f18fbf39423befa8b84c15c03ef2206
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-11-02 13:30:43 +0300

    2 часа кнопка для видео

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
 
 // ---------------------------------
 
